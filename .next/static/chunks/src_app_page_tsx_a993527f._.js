3e195f9d76aa5fd1b441030044fe00453f722fbd
Sultan Daffa Fattarsyah <113498070+SultanDF@users.noreply.github.com>
2025-07-24 17:57:19 +0700

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1753354639521

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_d066039d._.js",
+  "static/chunks/node_modules_8bdbce32._.js"
+],
+    source: "dynamic"
+});
